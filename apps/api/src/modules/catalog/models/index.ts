0193eb4bcc6df19f49acb7f0af9e6453a7b652db
hayle01 <mabdirahim832@gmail.com>
2026-07-31 20:40:35 +0300

    feat(catalog): export catalog models

---
+++ apps/api/src/modules/catalog/models/index.ts
@@ -0,0 +1,4 @@
+export { CategoryModel } from './Category.js';
+export { UnitModel } from './Unit.js';
+export { ProductModel } from './Product.js';
+export { ProductBarcodeModel } from './ProductBarcode.js';
