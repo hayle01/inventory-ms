c168426c2b93125713af703bb9ddc26c90dcad14
hayle01 <mabdirahim832@gmail.com>
2026-07-31 20:40:30 +0300

    feat(organization): export organization models

---
+++ apps/api/src/modules/organization/models/index.ts
@@ -0,0 +1,4 @@
+export { Organization } from './Organization.js';
+export { DepartmentModel } from './Department.js';
+export { WarehouseModel } from './Warehouse.js';
+export { StorageLocationModel } from './StorageLocation.js';
